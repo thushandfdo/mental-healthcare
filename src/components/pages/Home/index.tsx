fcda2633ffb3b7051fc6ae9399f2fb2cff4b595a
Thushan D. Fernando <thushandfdo@gmail.com>
2025-04-26 15:09:49 +0530

    Change Home Page button to primary

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -4,14 +4,14 @@ import { Button } from "@/components/atoms/Button";
 
 export const Home = () => {
     return (
-        <div className="">
-            <div className="container mx-auto flex h-screen items-center justify-center">
+        <div className="container flex items-center justify-center h-screen mx-auto">
+            <div className="flex flex-col items-center justify-center w-full gap-5">
                 <div className="text-center">
                     <h1 className="text-4xl font-bold text-sky-600">Welcome to Our Website</h1>
                     <p className="mt-4 text-lg text-gray-700">This is a simple home page.</p>
                 </div>
+                <Button onClick={() => console.log("Button Cliked...!")} label="Click Me" type="primary" />
             </div>
-            <Button onClick={() => console.log("Button Cliked...!")} label="Click Me" />
         </div>
     );
 };
